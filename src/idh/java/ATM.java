a86f89b1c6f76e8a616a926acd135394c10835fe
tinatomic <tinatomic@mbp-von-tina.fritz.box>
2023-05-30 22:07:05 +0200

    I tried

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,6 +1,3 @@
-package idh.java;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -26,7 +23,7 @@ public class ATM  {
 		while (true) {
 			try {
 				System.out.print("Enter your account number: ");
-				int accountNumber = Integer.parseInt(br.readLine());
+				Object accountNumber = br.readLine();
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(accountNumber, amount);
@@ -37,7 +34,7 @@ public class ATM  {
 		}
 	}
 
-	public void cashout(int accountNumber, int amount) {
+	public void cashout(Object accountNumber, int amount) {
 		// check for cash in the ATM
 		if (amount > cash) {
 			System.out.println("Sorry, not enough cash left.");
@@ -79,12 +76,13 @@ public class ATM  {
 	 * @param id
 	 * @return
 	 */
-	protected Account getAccount(int id) {
+	protected Account getAccount(Object id) {
 		for (Account account : bank) {
-			if (account.getId() == id) 
+			if (account.getId().equals(id));
 				return account;
 		}
 		return null;
 	}
 
 }
+
--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,61 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
 
 public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
-
+	static Map<Object, Account> map = new HashMap<Object, Account>();
+	static int length = 5;
+	static Account[] accounts = new Account[length];
+	
 	public Bank() {
-		// create accounts with varying balances
 		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+		int amount =  random.nextInt(1000);
+		for (int i = 0; i < length; i++) {
+			Account account = new Account(i, amount);
+			map.put(i, account);
+			accounts[i] = account;
 		}
 	}
-	
 	@Override
 	public Iterator<Account> iterator() {
 		return new AccountIterator(accounts);
 	}
 	
-	public Account getAccount(int number) {
-		// TODO: Implement
+	public static Account getAccount(Object number) {
+		for(int i = 0; i < map.size(); i++) {
+			if(number.equals(map.get(number).id)){
+				Account account = map.get(number);
+				return account;
+			}else {
+				System.out.println("Account nicht gefunden");
+			}
+		}
 		return null;
 	}
+	
+	public static void changeNumber(Object currentNumber, Object newNumber) {
+		Account account = getAccount(currentNumber);
+		account.setId(newNumber);
+		System.out.println(account.id);
 
+	}
+	
+	public static void main(String[] args) {
+		//Aufgabe 1
+		Bank bank = new Bank();
+		System.out.println(map.get(0));
+		System.out.println(accounts[0]);
+		System.out.println(getAccount(3));
+		
+		//Aufgabe 2
+		changeNumber(2, "💸");
+		changeNumber(3, "cat_haz_cash");
+		changeNumber(4, "1234@5678");
+		
+	}
 }
+
