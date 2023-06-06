af87e06c2e32cef6ac0c534653c6e973fac0b594
sophi <sophi@klick-klick-klick-tipp-tipp>
2023-06-06 15:48:13 +0200

    Task 1 in process (Trying out entrySet)

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,45 @@
 package idh.java;
 
-
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.Map.Entry;
 
 public class Bank implements Iterable<Account> {
 	Account[] accounts = new Account[5];
+	Map<Integer, Account> hmaccount = new HashMap<>();
 
 	public Bank() {
 		// create accounts with varying balances
 		Random random = new Random();
 		for (int i = 0; i < accounts.length; i++) {
 			accounts[i] = new Account(i, random.nextInt(1000));
+
+			// using loop to create
+			hmaccount.put(accounts[i].id, accounts[i]);
+
+			//
+			hmaccount.entrySet();
+			Set<Entry<Integer, Account>> entrySet = hmaccount.entrySet();
+			for (Entry<Integer, Account> e : entrySet) {
+				System.out.println(e.getKey() + " | " + e.getValue());
+
+			}
 		}
+
 	}
-	
+
 	@Override
 	public Iterator<Account> iterator() {
 		return new AccountIterator(accounts);
 	}
-	
+
+	// Implement getAccount(int) to get account off account number
 	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+		return hmaccount.get(number);
 	}
 
 }
