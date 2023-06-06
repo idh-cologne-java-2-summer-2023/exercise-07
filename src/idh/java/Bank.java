6aef52acd6af75f3c258e4b3ef869c1c3bc91b01
javi1337 <jan@pulsarstudio.de>
2023-06-06 13:19:02 +0200

    Aufgabe 1 first commit

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,28 +1,32 @@
 package idh.java;
 
-
 import java.util.Iterator;
 import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Bank implements Iterable<Account> {
+	private Map<Integer, Account> accMap = new HashMap<Integer, Account>();
 	Account[] accounts = new Account[5];
 
 	public Bank() {
 		// create accounts with varying balances
 		Random random = new Random();
 		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+			Account account = new Account(i, random.nextInt(1000));
+			accounts[i] = account;
+			this.accMap.put(i, accounts[i]);
 		}
 	}
-	
+
 	@Override
 	public Iterator<Account> iterator() {
 		return new AccountIterator(accounts);
 	}
-	
+
 	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+		return this.accMap.get(number);
+
 	}
 
 }
